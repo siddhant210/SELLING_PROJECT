11a39b12964f1cb7734d15073641066d4fc23162
Siddhantt06 <crce.9966.ce@gmail.com>
2026-03-19 17:28:06 +0530

    fix vercel config

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const { userRouter } = require("./routes/user");
-const { courseRouter } = require("./routes/course");
+
 const { adminRouter } = require("./routes/admin");
 
 const app = express();
